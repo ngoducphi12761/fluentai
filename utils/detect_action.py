ce524750d3d61ce1117fdfaba72d7510f7b8e8e6
ngoducphi12761 <cfdphind@gmail.com>
2025-04-28 21:27:47 -0500

    detecting action if the user query is an action-type command - needs JSON Action Plan

---
+++ utils/detect_action.py
@@ -0,0 +1,14 @@
+def is_action_command(user_query: str) -> bool:
+    """
+    Detect if the user query is an action-type command (needs JSON Action Plan),
+    or a general question (needs natural answer).
+    """
+    action_keywords = [
+        "set", "update", "change", "switch", 
+        "run", "configure", "assign", "modify",
+        "make it run"
+    ]
+
+    user_query_lower = user_query.lower()
+    
+    return any(keyword in user_query_lower for keyword in action_keywords)
